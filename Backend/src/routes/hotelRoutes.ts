4802916b95f5ddbd6b24d03e163fac0c779a78ee
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2025-04-08 15:34:59 +0530

    removed dummy hotels

--- Backend/src/routes/hotelRoutes.ts
+++ Backend/src/routes/hotelRoutes.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
-import {
-  getAllHotels,
-  getHotelById,
-  getDummyHotels,
-} from "../controller/hotelController";
+import { getAllHotels, getHotelById } from "../controller/hotelController";
 
 const hotelRouter = Router();
 
 hotelRouter.get("/", getAllHotels);
-hotelRouter.get("/dummy", getDummyHotels);
 hotelRouter.get("/:id", getHotelById);
 
 export default hotelRouter;
